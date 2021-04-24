446e436242a0b2571b33d1db254ad5ff6760e65d
adalbertopc <adalbertinho90@gmail.com>
2021-04-24 16:12:27 -0700

    feat: add password encrypt to post route

--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
+const passwordEncrypt = require('../middlewares/passwordEncrypt');
 
 router.get('/user', UserController.getAll);
-router.post('/user', UserController.insert);
+router.post('/user', passwordEncrypt, UserController.insert);
 
 module.exports = router;
