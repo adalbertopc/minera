017d2584920d4900f3097394fdfc9d4ac52d7229
adalbertopc <adalbertinho90@gmail.com>
2021-04-24 16:10:03 -0700

    build: add dotenv dependency

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 require('./database/Connection');
