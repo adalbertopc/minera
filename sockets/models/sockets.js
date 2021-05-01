884b67664a2332d1d7e67246ba92e11859ce1e44
adalbertopc <adalbertinho90@gmail.com>
2021-05-01 10:19:12 -0700

    fix: remove unnecessary socket event

--- sockets/models/sockets.js
+++ sockets/models/sockets.js
@@ -14,7 +14,6 @@ class Sockets {
 		// On connection
 		io.on('connection', (socket) => {
 			console.log('Cliente conectado');
-			//connection event
 
 			socket.on('change-traffic-light', ({ id, state }) => {
 				io.emit('update-traffic-lights', { id, state: !state });
@@ -29,41 +28,10 @@ class Sockets {
 				const carro = this.marcadores
 					.marcadoresActivos()
 					.find((elemento) => elemento.id == idCarro);
-				console.log(carro, 'si es merengue');
-				console.log(new Carga(carro.id, carro.conductor, infoCarga));
-			});
-
-			socket.on('info-sona', (objSona) => {
-				console.log(objSona);
-				//peticionPostCongestions(objSona);
-				// -------
-
-				//  fetch('https://7a226e8f6920.ngrok.io/api/congestions/', {
-				//      method: 'POST',
-				//      body: objSona
-				//
-				//   });
-
-				//axios.get('https://984b11671680.ngrok.io/api/congestions/')
-				//.then(response => {
-				//console.log(response.data)
-				//})
-				//.catch(error => {
-				//console.log(error)
-				//});
-
-				axios.post('https://984b11671680.ngrok.io/api/congestions/', objSona)
-					.then((response) => {
-						console.log(response.data);
-					})
-					.catch((error) => {
-						console.log(error);
-					});
 			});
 
 			socket.on('marcador-nuevo', (marcador) => {
 				this.marcadores.agregarMarcador(marcador);
-				console.log(marcador);
 				socket.broadcast.emit('marcador-nuevo', marcador);
 			});
 
@@ -75,8 +43,6 @@ class Sockets {
 			socket.on('sona-nueva', (sona) => {
 				if (!(sona.type === null || sona.type === undefined || !sona.type)) {
 					this.sonas.agregarSona(sona);
-					console.log(this.sonas, 'sonasss');
-					console.log(sona, ' desde sk');
 					socket.broadcast.emit('sona-nueva', sona);
 				}
 			});
