d567e9f6164d56e6988f0b11027ea68859d99eb8
adalbertopc <adalbertinho90@gmail.com>
2021-05-02 16:13:30 -0700

    feat: add verify token to congestions routes

--- api/routes/TrafficCongestionRoutes.js
+++ api/routes/TrafficCongestionRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const TrafficCongestionController = require('../controllers/TrafficCongestionController');
+const verifyToken = require('../middlewares/verifyToken');
 
-router.get('/congestions', TrafficCongestionController.getAll);
-router.post('/congestions', TrafficCongestionController.insert);
+router.get('/congestions', verifyToken, TrafficCongestionController.getAll);
+router.post('/congestions', verifyToken, TrafficCongestionController.insert);
 module.exports = router;
