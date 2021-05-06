ed7207b2348db60f9430acd9a239297b490a04d0
adalbertopc <adalbertinho90@gmail.com>
2021-05-06 13:31:15 -0700

    feat: add token verification

--- api/routes/VehicleRoutes.js
+++ api/routes/VehicleRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const VehicleController = require('../controllers/VehicleController');
+const verifyToken = require('../middlewares/verifyToken');
 
-router.get('/vehicles', VehicleController.getAll);
-router.post('/vehicles', VehicleController.insert);
+router.get('/vehicles', verifyToken, VehicleController.getAll);
+router.post('/vehicles', verifyToken, VehicleController.insert);
 module.exports = router;
